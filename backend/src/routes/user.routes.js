09224058f62fcad294832747dd4cc1b6be1d3064
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-03-05 18:13:12 +0530

    added login,register and getuser route. added Api Error, Response and AsyncHandler

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -7,3 +7,13 @@ import {
 } from '../controllers/user.controller.js'
 
 const router = Router()
+
+router.post('/login', loginUser)
+
+//secured routes
+router.post('/register', registerUser)
+router.get('/getUser', protect, getUserInfo)
+
+
+
+export default router
