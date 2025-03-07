90f6b02336545fbe3dee88245af98726b4f691ff
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-03-08 00:45:21 +0530

    add expense management functionality; implement add, get, delete, and download Excel features for expenses

--- backend/src/controllers/expense.controller.js
+++ backend/src/controllers/expense.controller.js
@@ -3,18 +3,104 @@ import { Expense } from '../models/Expense.model.js'
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import path from "path";
+import * as xlsx from 'xlsx';
 
-const addExpense = asyncHandler(async(req, res)=>{
+//Add income source
+const addExpense = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
 
+    const { icon, category, amount, date } = req.body
+
+    if (!category || !amount || !date) {
+        throw new ApiError("All fields are required")
+    }
+
+    const newExpense = new Expense({
+        userId,
+        icon,
+        category,
+        amount,
+        date: new Date(date)
+    })
+    await newExpense.save();
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, newExpense, "New Expense Added Successfully")
+    )
 })
-const getAllExpense = asyncHandler(async(req, res)=>{
 
+// get all income source
+const getAllExpense = asyncHandler(async (req, res) => {
+
+    const userId = req.user._id
+
+    const expense = await Expense.find({userId}).sort({date: -1})
+
+    if(!expense){
+            throw new ApiError(404, "Expense not found")
+        }
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, expense, "Fetched all Income sources")
+        )
 })
-const deleteExpense = asyncHandler(async(req, res)=>{
 
+//delete Expense
+const deleteExpense = asyncHandler(async (req, res) => {
+
+    await Expense.findByIdAndDelete(req.params.id);
+
+    res.status(200)
+    .json(
+        new ApiResponse(200,{},"Expense Deleted Successfully")
+    )
 })
-const downloadExpenseExcel = asyncHandler(async(req, res)=>{
 
+// download Expense Excel
+const downloadExpenseExcel = asyncHandler(async (req, res) => {
+    const userId = req.user._id
+    
+        const expense = await Expense.find({userId}).sort({date: -1})
+    
+        if (!expense || expense.length === 0) {
+            throw new ApiError(404, "No expense records found");
+        }
+    
+        //preparing data for excel
+        const data = expense.map((item) => ({
+            Source : item.source,
+            category: item.category,
+            Amount: item.amount,
+            Date: item.date
+        }))
+    
+        const wb = xlsx.utils.book_new();
+        const ws = xlsx.utils.json_to_sheet(data);
+        xlsx.utils.book_append_sheet(wb, ws, "expense");
+        xlsx.writeFile(wb, "expense_details.xlsx")
+    
+        // Save the Excel file temporarily
+        const filePath = path.join(process.cwd(), "temp_expense_details.xlsx");
+        xlsx.writeFile(wb, filePath);
+    
+        
+        const excelExpenseFile = uploadOnCloudinary(filePath)
+    
+        if(!excelExpenseFile){
+            throw new ApiError ("excelExpenseFile file is require")
+        }
+        const excelfileURL = excelExpenseFile.url
+    
+        return res.download('expense_details.xlsx',excelfileURL,  (err) => {
+            if (err) {
+                throw new ApiError(500, "Failed to download expense file");
+            }
+        })
 })
 
 
@@ -22,6 +108,6 @@ const downloadExpenseExcel = asyncHandler(async(req, res)=>{
 export {
     addExpense,
     getAllExpense,
-        deleteExpense,
-        downloadExpenseExcel
+    deleteExpense,
+    downloadExpenseExcel
 }
--- backend/src/models/Expense.model.js
+++ backend/src/models/Expense.model.js
@@ -2,18 +2,17 @@ import mongoose, { Schema } from 'mongoose';
 
 const expenseSchema = new Schema({
     userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        },
-        icon: {
-            type: String,
-        },
-        category: {
-            type: String,  //Example: food, Rent , Fule
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required: true
-        }
-        ,
+    },
+    icon: {
+        type: String,
+    },
+    category: {
+        type: String,  //Example: food, Rent , Fule
+        required: true
+    },
     amount: {
         type: Number,
         required: true
@@ -22,6 +21,6 @@ const expenseSchema = new Schema({
         type: Date,
         default: Date.now
     },
-},{timestamps: true})
+}, { timestamps: true })
 
 export const Expense = mongoose.model("Expense", expenseSchema)
